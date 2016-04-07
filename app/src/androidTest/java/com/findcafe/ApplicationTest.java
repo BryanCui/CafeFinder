653c21454182b267662c38b201c35226c1338ecb
Tianying Cui <cuitianying@gmail.com>
2016-04-07 20:27:51 +1000

    Add tests

--- app/src/androidTest/java/com/findcafe/ApplicationTest.java
+++ app/src/androidTest/java/com/findcafe/ApplicationTest.java
@@ -2,13 +2,67 @@ package com.findcafe;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
-import com.robotium.solo.Solo;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.junit.*;
+
+import java.util.List;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
 public class ApplicationTest extends ApplicationTestCase<Application> {
+    private DataHandler dataHandlerTest;
+    private DatabaseHelper databaseHelperTest;
     public ApplicationTest() {
         super(Application.class);
+        dataHandlerTest = new DataHandler();
     }
+
+    @Test
+    public void testGetDataFromServer() {
+        double latitudeTest = -37.7980844;
+        double longitudeTest = 144.9589147;
+        JSONArray arrayTest = dataHandlerTest.getCafeDataFromServer(latitudeTest, longitudeTest);
+        assertTrue(arrayTest.length() > 0);
+    }
+
+
+    @Test
+    public void testCafeInfoHandle() {
+        try {
+            double latitudeTest = -37.7980844;
+            double longitudeTest = 144.9589147;
+            JSONArray arrayTest = dataHandlerTest.getCafeDataFromServer(latitudeTest, longitudeTest);
+
+            Cafe cafe = dataHandlerTest.cafeInfoHandle(arrayTest.getJSONObject(0));
+            assertTrue(cafe != null);
+            assertTrue(cafe.getCafeId() != null);
+            assertTrue(cafe.getCafeName() != null);
+            assertTrue(cafe.getCafeAddress() != null);
+            assertTrue(cafe.getCafeLatitude() != 0.0);
+            assertTrue(cafe.getCafeLongitude() != 0.0);
+            assertTrue(cafe.getCafeDistance() != 0);
+        } catch (JSONException e) {
+            e.printStackTrace();
+            assertTrue(false);
+        }
+    }
+
+//    @Test
+//    public void testDataInsert() {
+//        try {
+//            double latitudeTest = -37.7980844;
+//            double longitudeTest = 144.9589147;
+//            JSONArray arrayTest = dataHandlerTest.getCafeDataFromServer(latitudeTest, longitudeTest);
+//
+//            dataHandlerTest.insertCafeInfoIntoDatabase(databaseHelperTest, arrayTest);
+//            List<Cafe> testList = dataHandlerTest.getCafeList(databaseHelperTest);
+//            assertEquals(testList.size(), arrayTest.length());
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//
+//    }
 }
---
+++ app/src/androidTest/java/com/findcafe/MainActivityTest.java
@@ -0,0 +1,64 @@
+package com.findcafe;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+import com.robotium.solo.Solo;
+
+import org.json.JSONArray;
+
+import java.util.List;
+
+/**
+ * Created by Tianying on 7/04/2016.
+ */
+public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
+
+    private Solo solo;
+
+    public MainActivityTest() {
+        super(MainActivity.class);
+    }
+
+    public MainActivityTest(Class<MainActivity> activityClass) {
+        super(activityClass);
+    }
+
+    @Override
+    public void setUp() throws Exception {
+        //setUp() is run before a test case is started.
+        //This is where the solo object is created.
+        solo = new Solo(getInstrumentation(), getActivity());
+    }
+
+    @Override
+    public void tearDown() throws Exception {
+        //tearDown() is run after a test case has finished.
+        //finishOpenedActivities() will finish all the activities that have been opened during the test execution.
+        solo.finishOpenedActivities();
+    }
+
+    public void testDatabase() throws Exception {
+        DataHandler dataHandlerTest = new DataHandler();
+        DatabaseHelper databaseHelperTest = new DatabaseHelper(getActivity());
+        try {
+            double latitudeTest = -37.7980844;
+            double longitudeTest = 144.9589147;
+            JSONArray arrayTest = dataHandlerTest.getCafeDataFromServer(latitudeTest, longitudeTest);
+
+            dataHandlerTest.insertCafeInfoIntoDatabase(databaseHelperTest, arrayTest);
+            List<Cafe> testList = dataHandlerTest.getCafeList(databaseHelperTest);
+            assertEquals(testList.size(), arrayTest.length());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void testUI() throws Exception {
+        solo.unlockScreen();
+        solo.waitForDialogToClose();
+        solo.assertCurrentActivity("Test current activity", MainActivity.class);
+        solo.clickOnText("Cafe Map");
+        solo.clickOnText("Cafe List");
+        solo.clickInList(1, 0);
+    }
+}
